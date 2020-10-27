a0f981bdb994cadc66dd63ff65e0c863f2517f06
MaximSungmo <ksm5318@naver.com>
2020-10-27 23:46:55 +0900

    서버구성

---
+++ 7장/start/src/search/component/Settings.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Button, Dropdown, Menu } from 'antd';
+import { SettingFilled } from '@ant-design/icons';
+
+/**
+ * 
+ * @param {object} param
+ * @param {()=> void} param.logout
+ */
+export default function Settings({ logout }) {
+    return (
+        <>
+            <Dropdown
+                overlay={
+                    <Menu>
+                        <Menu.Item onClick={logout}>
+                            Logout
+                        </Menu.Item>
+                    </Menu>
+                }
+                trigger={['click']}
+                placement="bottomRight"
+            >
+                <Button shape="circle" icon={<SettingFilled />} />
+            </Dropdown>
+        </>
+    );
+}
---
+++ 7장/start/src/search/container/Search.js
@@ -0,0 +1,24 @@
+import { Col, Row, Typography } from 'antd';
+import React from 'react';
+import SearchInput from '../component/SearchInput';
+import Settings from '../component/Settings';
+
+export default function Search() {
+    return (
+    <>
+        <Row justify="end" style={{ padding: 20 }}>
+            <Col>
+                <Settings logout={()=>{}} />
+            </Col>
+        </Row>
+        <Row justify="center" style={{ marginTop: 100 }}>
+            <Col>
+                <Typography.Title style={{ fontFamily: 'Caligraphy' }}>찾아야한다</Typography.Title>
+            </Col>
+        </Row>
+        <Row justify="center" style={{ marginTop: 50 }}>
+            <Col span={12}><SearchInput/></Col>
+        </Row>
+    </>
+    );
+}
